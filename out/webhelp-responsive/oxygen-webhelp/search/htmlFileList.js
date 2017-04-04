5aefa3f0fc238592c4cc6b0b6a39fc535952e825
haleyl5 <haleylucey@Haleys-MacBook-Air.local>
2017-04-04 19:41:50 -0400

    haley

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,14 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "c-disclaimer.html";
+fl["1"]= "c-intro.html";
+fl["2"]= "l-cleaningkitchen.html";
+fl["3"]= "l-wholefoods.html";
+fl["4"]= "m-cranberry.html";
+fl["5"]= "m-final.html";
+fl["6"]= "m-settingtable.html";
+fl["7"]= "m-websites.html";
+fl["8"]= "t-choosingday.html";
+fl["9"]= "t-quiche.html";
+fl["10"]= "t-tots.html";
+var doStem = false;searchLoaded = true;
